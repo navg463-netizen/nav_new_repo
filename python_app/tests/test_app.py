cd366a648170eeb087d584169f71954edc994245
navg463@gmail.com <navg463@gmail.com>
2025-10-29 07:07:10 +0000

    iupdated

--- python_app/tests/test_app.py
+++ python_app/tests/test_app.py
@@ -1,3 +1,18 @@
+# import pytest
+# from app import app, redis_client
+# import sys, os
+# sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+
+
+# @pytest.fixture
+# def client():
+#     app.config['TESTING'] = True
+#     return app.test_client()
+
+# def test_home(client):
+#     response = client.get('/')
+#     assert response.status_code == 200
+
 import pytest
 import json
 from unittest.mock import MagicMock, patch
